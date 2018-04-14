a3034a6e1f75fd16c5b2dae0d665c5b68b494886
Evgeniy <qtug@yandex.com>
2018-04-14 19:09:47 +0300

    done

--- QtOpenGLDB/scene.cpp
+++ QtOpenGLDB/scene.cpp
@@ -12,7 +12,7 @@ void Scene::initializeGL() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     connect(this, SIGNAL(frameSwapped()), this, SLOT(update()));
     glEnable(GL_CULL_FACE);
-    glLineWidth(2.0f);
+    glLineWidth(0.5f);
 
     m_program = new QOpenGLShaderProgram();
     m_program->addShaderFromSourceFile(QOpenGLShader::Vertex, ":/shaders/simple.vert");
@@ -34,8 +34,8 @@ void Scene::initializeGL() {
 
     m_program->enableAttributeArray(0);
     m_program->enableAttributeArray(1);
-    m_program->setAttributeBuffer(0, GL_FLOAT, 0, Vertex::PositionTupleSize, Vertex::stride());
-    m_program->setAttributeBuffer(1, GL_FLOAT, 3 * sizeof(float), Vertex::ColorTupleSize, Vertex::stride());
+    m_program->setAttributeBuffer(0, GL_FLOAT, 0, 3, 24);
+    m_program->setAttributeBuffer(1, GL_FLOAT, 12, 3, 24);
 
     cubesVBO.release();
     cubeVAO.release();
@@ -45,28 +45,22 @@ void Scene::initializeGL() {
     coordsVAO.create();
     coordsVAO.bind();
 
-    u_modelToWorld = m_program->uniformLocation("modelToWorld");
-    u_worldToView = m_program->uniformLocation("worldToView");
-
     coordsVBO.create();
     coordsVBO.setUsagePattern(QOpenGLBuffer::StaticDraw);
     coordsVBO.bind();
 
     m_program->enableAttributeArray(0);
     m_program->enableAttributeArray(1);
-    m_program->setAttributeBuffer(0, GL_FLOAT, 0, Vertex::PositionTupleSize, Vertex::stride());
-    m_program->setAttributeBuffer(1, GL_FLOAT, 3 * sizeof(float), Vertex::ColorTupleSize, Vertex::stride());
+    m_program->setAttributeBuffer(0, GL_FLOAT, 0, 3, 24);
+    m_program->setAttributeBuffer(1, GL_FLOAT, 12, 3, 24);
 
-    coordsVBO.allocate(3 * 2 * 121 * sizeof(Vertex));
+    coordsVBO.allocate(17424);
     auto coordsVec = getCoords();
-    for (int i = 0 ; i < 3; i++) {
-        auto ptr = coordsVBO.mapRange(2 * 121 * sizeof(Vertex) * i, 2 * 121 * sizeof(Vertex), QOpenGLBuffer::RangeInvalidate | QOpenGLBuffer::RangeWrite);
-        auto arr = coordsVec[i].data();
-
-        //memcpy(ptr, coordsVec[i].data(), 2 * 121 * sizeof(Vertex));
+    for (unsigned i = 0 ; i < 3; i++) {
+        auto ptr = coordsVBO.mapRange(i * 5808, 5808, QOpenGLBuffer::RangeInvalidate | QOpenGLBuffer::RangeWrite);
+        memcpy(ptr, coordsVec[i].data(), 5808);
+        coordsVBO.unmap();
     }
-//    coordsVBO.unmap();
-
 
     coordsVBO.release();
     coordsVAO.release();
@@ -90,11 +84,11 @@ void Scene::paintGL() {
     }
     cubeVAO.release();
 
-//    coordsVAO.bind();
-//    m_program->setUniformValue(u_worldToView, QMatrix4x4());
-//    m_program->setUniformValue(u_modelToWorld, QMatrix4x4());
-//    glDrawArrays(GL_LINES, 0, 3 * 2 * 121);
-//    coordsVAO.release();
+    coordsVAO.bind();
+    m_program->setUniformValue(u_worldToView, m_projection);
+    m_program->setUniformValue(u_modelToWorld, m_transform.toMatrix());
+    glDrawArrays(GL_LINES, 0, 726);
+    coordsVAO.release();
 
     m_program->release();
 }
@@ -129,8 +123,8 @@ void Scene::reloadSetup() {
     for (int i = 0 ; i < this->figures.first.size(); i++) {
         auto ptr = cubesVBO.mapRange(864 * i, 864, QOpenGLBuffer::RangeInvalidate | QOpenGLBuffer::RangeWrite);
         memcpy(ptr, this->figures.first[i].data(), 864);
+        cubesVBO.unmap();
     }
-    cubesVBO.unmap();
     cubesVBO.release();
     cubeVAO.release();
     m_program->release();
@@ -138,32 +132,27 @@ void Scene::reloadSetup() {
 
 std::vector<std::vector<Vertex>> Scene::getCoords() {
     std::vector<std::vector<Vertex>> coords(3);
-    std::vector<QVector3D> colors = {
-        QVector3D(1.0f, 0.0f, 0.0f),
-        QVector3D(0.0f, 1.0f, 0.0f),
-        QVector3D(0.0f, 0.0f, 1.0f),
-    };
     for (int j = -5; j < 6; j++) {
         for (int k = -5; k < 6; k++) {
             coords[0].push_back(Vertex(
-                            QVector3D(-1.0f, float(j), float(k)),
-                            colors[0])
+                            QVector3D(-5.0f, float(j), float(k)),
+                            QVector3D(1.0f, 0.0f, 0.0f))
             );
             coords[0].push_back(Vertex(
-                            QVector3D(1.0f, float(j), float(k)),
-                            colors[0])
+                            QVector3D(5.0f, float(j), float(k)),
+                            QVector3D(1.0f, 0.0f, 0.0f))
             );
         }
     }
     for (int j = -5; j < 6; j++) {
         for (int k = -5; k < 6; k++) {
             coords[1].push_back(Vertex(
-                            QVector3D(float(j), -1.0f, float(k)),
-                            colors[1])
+                            QVector3D(float(j), -5.0f, float(k)),
+                            QVector3D(0.0f, 1.0f, 0.0f))
             );
             coords[1].push_back(Vertex(
-                            QVector3D(float(j), 1.0f, float(k)),
-                            colors[1])
+                            QVector3D(float(j), 5.0f, float(k)),
+                            QVector3D(0.0f, 1.0f, 0.0f))
             );
         }
     }
@@ -171,12 +160,12 @@ std::vector<std::vector<Vertex>> Scene::getCoords() {
     for (int j = -5; j < 6; j++) {
         for (int k = -5; k < 6; k++) {
             coords[2].push_back(Vertex(
-                            QVector3D(float(j), float(k), -1.0f),
-                            colors[2])
+                            QVector3D(float(j), float(k), -5.0f),
+                            QVector3D(0.0f, 0.0f, 1.0f))
             );
             coords[2].push_back(Vertex(
-                            QVector3D(float(j), float(k), 1.0f),
-                            colors[2])
+                            QVector3D(float(j), float(k), 5.0f),
+                            QVector3D(0.0f, 0.0f, 1.0f))
             );
         }
     }
